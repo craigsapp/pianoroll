b9b881d1c3407579e5eab700d1b038cd9d84e78a
Craig Sapp <craigsapp@gmail.com>
2015-10-06 19:21:49 -0700

    Intialized repository.

---
+++ index.md
@@ -0,0 +1,11 @@
+---
+layout: default
+title: Piano Roll
+---
+
+<h1> Piano Roll Stuff </h1>
+
+<ol>
+<li> <a href="resolution">Resolution tests</a>
+</ol>
+
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Fri Sep 18 21:15:35 PDT 2015
+// Last Modified: Mon Sep 21 19:27:35 PDT 2015
+// Filename:      scripts/main.js
+// Syntax:        JavaScript 1.8.5/ECMAScript 5.1
+// vim:           ts=3 hlsearch
+//
+// Description:   Main functions for managing DRM.
+//
+
